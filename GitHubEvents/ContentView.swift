7ab79620814ddb16ec464a0da010c7d2f5a1c66e
Sebastien Thibert <sebastien.thibert@marks-and-spencer.com>
2022-07-26 06:44:51 +0100

    Initial Commit

---
+++ GitHubEvents/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubEvents
+//
+//  Created by Sebastien Thibert on 26/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubEvents/GitHubEventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubEventsApp.swift
+//  GitHubEvents
+//
+//  Created by Sebastien Thibert on 26/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitHubEventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
